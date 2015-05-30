dc8be530d1ae8b627a378e73969a45bb47e06c75
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-05-30 19:39:22 +0200

    Incredibly rudimentary embryo of a load and save mechanism (and corresponding data model).

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ function Container(params)
   
   params = params || {};
   
-  this.child_nodes = [];
+  this.child_nodes = params.child_nodes || [];
 }
 
 Container.prototype = new Node();
@@ -54,14 +54,6 @@ function DocumentController(element, options)
   this.options = options || {};
 }
 
-DocumentController.prototype.save = function(element)
-{
-  console.log('DocumentController::save()', 'firstChild:', element.firstChild);
-  for (var child = element.firstChild; !!child; child = child.nextSibling) {
-    console.log('  child:', child.nodeType +':', child);
-  }
-}
-
 DocumentController.prototype.onKeyDown = function(e)
   // e:       jQuery-wrapped keydown event
   // returns: return false will stop default action AND propagation (see jQuery)
@@ -81,9 +73,34 @@ DocumentController.prototype.queueSave = function()
   window.setTimeout(function() { this.save(); }.bind(this), 1); 
 }
 
+DocumentController.prototype.load = function(doc)
+{
+  console.log('DocumentController::load()', doc);
+  
+  var frag = document.createDocumentFragment();
+  
+  for (var i = 0; i < doc.child_nodes.length; i ++) {
+    var child = doc.child_nodes[i];
+    console.log('child:', child);
+    frag.appendChild( elementFromParagraph(child) );
+  }
+  
+  this.element.innerHtml = '';
+  this.element.appendChild(frag);
+  
+  //-----------------------
+  
+  function elementFromParagraph(p)
+  {
+    var el = document.createElement('p');
+    el.innerText = p.content;
+    return el;
+  }
+}
+
 DocumentController.prototype.save = function()
 {
-  console.log('save()', this);
+  console.log('DocumentController::save()');
   
   var newdoc = new Document();
   
@@ -107,41 +124,41 @@ DocumentController.prototype.save = function()
 
 ko.bindingHandlers.gpcSimpleDocEditor = {
   
-    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-        // This will be called when the binding is first applied to an element
-        // Set up any initial state, event handlers, etc. here
-        
-        console.log('this:', this);
-        
-        var controller = new DocumentController(element);
-        
-        var value = ko.unwrap(valueAccessor());
-        $(element)
-          .addClass('gpc-simpledoc-editor')
-          .attr('contenteditable', true)
-          .append( $('<p>').text(value.tagLine) ) // TODO: real translation
-          .on('keydown', function(e) {
-            return controller.onKeyDown(e);
-          })
-          .on('blur', function(e) {
-            console.log('blur:', e);
-            controller.save(element);
-          })
-          .on('input', function(e) {
-            console.log('input event:', e);
-          })
-    },
-    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-        // This will be called once when the binding is first applied to an element,
-        // and again whenever any observables/computeds that are accessed change
-        // Update the DOM element based on the supplied values here.
-    }
+  init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+    // This will be called when the binding is first applied to an element
+    // Set up any initial state, event handlers, etc. here
+    
+    var controller = new DocumentController(element);
+    
+    $(element)
+      .addClass('gpc-simpledoc-editor')
+      .attr('contenteditable', true)
+      .on('keydown', function(e) {
+        return controller.onKeyDown(e);
+      })
+      .on('blur', function(e) {
+        console.log('blur:', e);
+        controller.save(element);
+      })
+      .on('input', function(e) {
+        console.log('input event:', e);
+      })
+      
+    controller.load( valueAccessor() );
+  },
+  update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+    // This will be called once when the binding is first applied to an element,
+    // and again whenever any observables/computeds that are accessed change
+    // Update the DOM element based on the supplied values here.
+  }
 };
 
-var my_doc = {
-  
-  tagLine: "My very first SimpleDoc document"
-};
+var my_doc = new Document({  
+  child_nodes: [
+    new Paragraph({content: "This is the first paragraph."}),
+    new Paragraph({content: "So that makes this the second paragraph"})
+  ]
+});
 
 window.onload = function() {
   ko.applyBindings(my_doc);
