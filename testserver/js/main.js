86244ccf9158789e670b998e47b49d3fd92cade8
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-06-09 23:22:35 +0200

    Correctly refreshing the index after saving changes to a document.

--- testserver/js/main.js
+++ testserver/js/main.js
@@ -7,46 +7,7 @@ var gsd = require('gpc-simpledoc');
 var SimpleDocEditor = gsd.Editor;
 var ko = gsd.ko;
 
-/*
-ko.observable({  
-    child_nodes: [
-      { _type_: 'PARAGRAPH', content: "This is the first paragraph." },
-      { _type_: 'PARAGRAPH', content: "So that makes this the second paragraph" }
-]})
-*/
-    
 gsd.init();
-
-/* Since documents are now method-less JSON objects, we need a few conveniences
-  to access their data.
- */ 
-var DocumentHelper = {
-  
-  getTagline: function(doc) {
-    
-    console.log('getTagline:', doc);
-    
-    var text;
-    
-    if (_.isString(doc.title) && doc.title.length > 0) {
-      text = doc.title;
-    }
-    else if (this.hasChildren(doc)) {
-      text = doc.child_nodes[0].content;
-      if (text.length > 80) text = text.slice(0, 80) + "...";
-    }
-    
-    if (!_.isString(text)) text = '(unnamed/empty document)';
-
-    console.log('text:', text);
-    
-    return text;
-  },
-  
-  hasChildren: function(doc) {
-    return doc.child_nodes && doc.child_nodes.length > 0;
-  }
-}
 
 var data = {
   
@@ -59,16 +20,6 @@ var data = {
   {
     console.log('saveDocument', data);
     
-    /*
-    ajax('/api/articles/new_uuid', {
-      complete: function(data) {
-        console.log('new_uuid:', data);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.log('error:', textStatus);
-      }
-    })
-    */
     /* ajax('/api/articles/new_uuid')
       .then( function(data) {
         console.log('new_uuid as promised:', data);
@@ -83,6 +34,7 @@ var data = {
     })
     .then( function(result) {
       console.log('success:', result);
+      reloadIndex();
     })
     .fail( function(err) {
       alert('Failed to store the document: ' + err);
@@ -92,28 +44,36 @@ var data = {
 
 ko.applyBindings(data);
 
-ajax('/api/articles', { dataType: 'json' })
-.then( function(result) {
-  console.log('Index:', result);
-  _.each(result, function(item) { 
-    console.log('item:', item); 
-    data.index.push( {
-      tagline: item.value.tagline, //DocumentHelper.getTagline(item),
-      load: function() {
-        ajax('/api/articles/'+item.id, { dataType: 'json' })
-          .then( function(doc) {
-            console.log('got the document:', doc);
-            data.docEditor.load(doc);
-          })
-          .fail( function(err) {
-            alert('Failed to load the document: ' + err);
-          })
-      }
-    });
-  })
-})
-.fail( function(err) {
-  alert('failed to obtain article list:' + err);
-})
+reloadIndex();
 
 //-------------------
+
+function reloadIndex()
+{
+  ajax('/api/articles', { dataType: 'json' })
+  .then( function(result) {
+    console.log('Index:', result);
+    var new_index = [];
+    _.each(result, function(item) { 
+      console.log('item:', item); 
+      new_index.push( {
+        tagline: item.value.tagline,
+        load: function() {
+          ajax('/api/articles/'+item.id, { dataType: 'json' })
+            .then( function(doc) {
+              console.log('got the document:', doc);
+              data.docEditor.load(doc);
+            })
+            .fail( function(err) {
+              alert('Failed to load the document: ' + err);
+            })
+        }
+      })
+    })
+    data.index(new_index);
+  })
+  .fail( function(err) {
+    alert('failed to obtain article list:' + err);
+  })
+}
+
