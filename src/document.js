4d53d006eb023d0b26ca49e9f72adf8b6fccb063
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-06-20 16:54:51 +0200

    Alternate dom to paragraph converter

--- src/document.js
+++ src/document.js
@@ -8,7 +8,8 @@ Registry.registerPlugin('document', 'toDOM', function(data) {
   
     var frag = document.createDocumentFragment();
     
-    for (var i = 0; i < data.child_nodes.length; i ++) {
+    for (var i = 0; i < data.child_nodes.length; i ++) 
+    {
       var child = data.child_nodes[i];
       frag.appendChild( elementFromParagraph(child) );
     } 
@@ -34,7 +35,8 @@ Registry.registerPlugin('document', 'fromDOM', function(container) {
       child_nodes: []
     };
     
-    for (var child = container.firstChild; !!child; child = child.nextSibling) {
+    for (var child = container.firstChild; !!child; child = child.nextSibling) 
+    {
       if (child.tagName === 'P') 
         doc.child_nodes.push( paragraphFromElement(child) );
       else
@@ -45,11 +47,34 @@ Registry.registerPlugin('document', 'fromDOM', function(container) {
     
     //------------------
     
-    function paragraphFromElement(p)
+    function paragraphFromElement(cont_elem)
     {
+      return {
+        content: nodeToText(cont_elem)
+      };
+
+      //------------
+      
+      function nodeToText(cont_elem)
+      {
+        var text = '';
+        for (var node = cont_elem.firstChild; !!node; node = node.nextSibling) 
+        {
+          if (node.nodeType == 1)
+            text += nodeToText(node) // recurse
+          else if (node.nodeType == 3) 
+            text += node.nodeValue;
+          else
+            throw new Error("nodeToText(): unsupported node type");
+        }
+        return text;
+      }
+      
+      /*
       // TODO: a real implementation
       // TODO: define and handle inline elements
       console.log('p.innerHTML:', p.innerHTML);
       return { content: p.innerHTML };
+      */
     }
 });
